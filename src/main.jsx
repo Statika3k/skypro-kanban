a378b95995c7ac11198332595a6c6277f1823f2c
Statika3k <shahanova.ns@gmail.com>
2025-11-23 12:18:20 +0300

    Перенесены глобальные стили

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { GlobalStyles } from './styles/GlobalStyles';
 import App from './App.jsx'
+import { ThemeProvider } from 'styled-components';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={{}}>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
+    
   </StrictMode>,
 )
